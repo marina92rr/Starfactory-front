dd24421f23faf24c11ede409d513ff69709d4299
marina92rr <110558101+marina92rr@users.noreply.github.com>
2025-06-14 10:49:34 +0200

    Update clientsApi.js

--- src/api/clientsApi.js
+++ src/api/clientsApi.js
@@ -8,7 +8,7 @@ const {VITE_API_URL} = getEnvVariables();
 const clientsApi = axios.create({
       //baseURL: 'http://localhost:4001'
       //baseURL: 'http://161.97.121.100/api'
-        baseUrl: 'https://starfactorysevillaadmin.com/api'
+        baseURL: 'https://starfactorysevillaadmin.com/api'
   });
 
 
