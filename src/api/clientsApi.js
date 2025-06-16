d8508b54f7ee9fe76500b14e8899b47165d968e6
marina92rr <marina92.rr@gmail.com>
2025-06-16 19:06:43 +0200

    api

--- src/api/clientsApi.js
+++ src/api/clientsApi.js
@@ -6,9 +6,8 @@ import { getEnvVariables } from "../helpers/getEnvVariables";
 const {VITE_API_URL} = getEnvVariables();
 
 const clientsApi = axios.create({
-      baseURL: 'http://localhost:4001'
-      //baseURL: 'http://161.97.121.100/api'
-        //baseURL: 'https://starfactorysevillaadmin.com/api'
+      //baseURL: 'http://localhost:4001'
+        baseURL: 'https://starfactorysevillaadmin.com/api'
   });
 
 
