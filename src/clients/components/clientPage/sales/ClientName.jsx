6622a47b72369147976eeaf673ca6f86a017a413
marina92rr <marina92.rr@gmail.com>
2025-09-02 11:53:47 +0200

    name enlace

--- src/clients/components/clientPage/sales/ClientName.jsx
+++ src/clients/components/clientPage/sales/ClientName.jsx
@@ -1,20 +1,30 @@
 
 import React, { useEffect } from 'react'
 import { useClientsStore } from '../../../../hooks/useClientsStore'
+import { useNavigate } from 'react-router-dom';
 
 export const ClientName = ({idClient}) => {
 
   const { getClientbyClientID, clientsName } = useClientsStore();
+  const navigate = useNavigate();
+
 
   useEffect(() => {
-    getClientbyClientID(idClient);
-  }, [idClient]);
+    getClientbyClientID(idClient)
+  }, [idClient])         // ⬅️ añade la dep. recomendada
+
 
   const client = clientsName[idClient];
 
+     // Cuando seleccionas un cliente, navegamos a /clients/:ID
+  const handleSelect = () => {
+    if (client?.idClient) navigate(`/${client.idClient}`)
+   
+  };
+
   return (
     
-    <div className='text-primary'> {client?.name} {client?.lastName} </div>
+    <a type='button' onClick={handleSelect} className='btn p-0 border-0 bg-transparent text-primary'> {client?.name} {client?.lastName} </a>
     
   )
 }
